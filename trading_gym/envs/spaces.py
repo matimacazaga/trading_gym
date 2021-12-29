6a6765649f151818c7b772275326ff321b7ff0db
Matias Macazaga <matimacazaga7@gmail.com>
2021-12-29 14:55:56 -0300

    Modified sample from space

--- trading_gym/envs/spaces.py
+++ trading_gym/envs/spaces.py
@@ -1,5 +1,7 @@
+from typing import List, Tuple
 import numpy as np
 from gym import Space
+import pandas as pd
 
 
 class PortfolioVector(Space):
@@ -7,20 +9,24 @@ class PortfolioVector(Space):
     Data structure for portfolio vector.
     """
 
-    def __init__(self, num_instruments):
+    def __init__(self, num_instruments: int, universe: List[str]):
 
         self.low = np.zeros(num_instruments, dtype=float)
 
         self.high = np.ones(num_instruments, dtype=float)  # * np.inf
 
-    def sample(self):
+        self.universe = universe
+
+    def sample(self) -> pd.Series:
         """
         Get a random sample of a portfolio vector.
         """
         _vec = np.random.uniform(0.0, 1.0, self.shape[0])
-        return _vec / np.sum(_vec)
+        _vec = _vec / np.sum(_vec)
+        _vec = pd.Series(_vec, index=self.universe)
+        return _vec
 
-    def contains(self, x: np.array, tolerance=1e-5):
+    def contains(self, x: np.array, tolerance: float = 1e-5) -> bool:
         """
         Checks if a vector x is in the space.
         """
@@ -30,11 +36,11 @@ class PortfolioVector(Space):
         return shape_predicate and range_predicate and budget_constraint
 
     @property
-    def shape(self):
+    def shape(self) -> Tuple[int]:
         return self.low.shape
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"PortfolioVector {self.shape}"
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         return np.allclose(self.low, other.low) and np.allclose(self.high, other.high)
