3142c7ffd3b2913d4703df3d8290614c6607391c
Matias Macazaga <matimacazaga7@gmail.com>
2021-12-30 11:33:56 -0300

    Testing cla agent

---
+++ trading_gym/testing/test_cla_agent.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+from ..utils.summary import stats
+from ..agents.cla import CLAAgent
+from ..envs.trading import TradingEnv
+import pickle
+from tqdm import tqdm
+
+
+def sim_hrp_agent(universe, window):
+
+    env = TradingEnv(
+        universe=universe,
+        cash=True,
+        start=datetime(2020, 1, 1),
+        end=datetime(2021, 12, 21),
+    )
+
+    agent = CLAAgent(action_space=env.action_space, window=window, J="max_sharpe")
+
+    env.register(agent)
+
+    ob = env.reset()
+
+    reward = 0
+
+    done = False
+
+    for _ in tqdm(range(env._max_episode_steps)):
+        if done:
+            break
+        agent.observe(ob, None, reward, done, None)
+        action = agent.act(ob)
+        ob, reward, done, _ = env.step({agent.name: action})
+        ob = ob
+
+    pickle.dump(
+        env.agents[agent.name],
+        open("./trading_gym/testing/tests_results/cla_agent_test.pickle", "wb"),
+    )
+
+    pickle.dump(
+        stats(env.agents[agent.name].rewards.iloc[WINDOW:].sum(axis=1)),
+        open("./trading_gym/testing/tests_results/cla_agent_stats.pickle", "wb"),
+    )
+
+
+if __name__ == "__main__":
+
+    UNIVERSE = ["BTCUSDT", "ETHUSDT", "MINAUSDT"]
+    WINDOW = 60
+    sim_hrp_agent(UNIVERSE, WINDOW)
